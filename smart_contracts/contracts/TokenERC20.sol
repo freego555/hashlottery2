8f5c9662d3c052a1d96621c9ecc20c8b399ff1ce
Egor Ternovoy <egor.smartit@gmail.com>
2018-10-06 14:03:26 +0300

    Token Created

---
+++ smart_contracts/contracts/TokenERC20.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.23;
+
+contract TokenERC20 {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 0;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    address public owner;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor(string tokenName, string tokenSymbol) public {
+        name = 'LotStock';
+        symbol = 'L$';
+        owner = msg.sender;
+    }
+
+    function supplyTokens() {
+        if (owner == msg.sender) {
+            totalSupply = 100000;
+            balanceOf[msg.sender] = totalSupply;
+        } else {
+            throw;
+        }
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal {
+        require(balanceOf[_from] >= _value);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public
+    returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public
+    returns (bool success) {
+        allowance[msg.sender][_spender] += _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function getOwnership() public view returns (uint8 percentage) {
+        return balanceOf[account] / totalSupply * 10000;
+    }
+}
