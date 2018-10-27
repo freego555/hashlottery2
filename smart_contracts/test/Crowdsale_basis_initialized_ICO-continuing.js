b87ba450414afa599d69f3a2599524d996f80365
Ihor Rohovets <freego555@gmail.com>
2018-10-27 06:24:57 +0300

    tests of crowdsale initialized and ICO continuing

--- smart_contracts/test/Crowdsale_basis_initialized_ICO-continuing.js
+++ smart_contracts/test/Crowdsale_basis_initialized_ICO-continuing.js
@@ -27,7 +27,7 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             let startICO = await crowdsale.startICO();
             //console.log(numBeforeEach);
             assert.isTrue(startICO > 0, 'startICO() isn\'t more than 0');
-        })
+        });
 
         it('startICOPlus2Days() should be startICO() + 2 days', async function(){
             let crowdsale = await Crowdsale.deployed();
@@ -40,7 +40,7 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             startICO = startICO.plus(twoDaysInSeconds);
 
             assert.isTrue(startICO.comparedTo(startICOPlus2Days) == 0, 'startICOPlus2Days() isn\'t startICO() + 2 days');
-        })
+        });
 
         it('deadline() should be startICO() + 7 days', async function(){
             let crowdsale = await Crowdsale.deployed();
@@ -53,7 +53,7 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             startICO = startICO.plus(sevenDaysInSeconds);
 
             assert.isTrue(startICO.comparedTo(deadline) == 0, 'deadline() isn\'t startICO() + 7 days');
-        })
+        });
 
         it('price() should be 0.25 Ether', async function(){
             let crowdsale = await Crowdsale.deployed();
@@ -65,7 +65,7 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
 //            console.log(web3.toWei(0.25));
 
             assert.isTrue(price == web3.toWei(0.25), 'price() isn\'t 0.25 Ether');
-        })
+        });
 
         it('amountTokensForSale() should be 40% of total supply of tokens', async function() {
             let crowdsale = await Crowdsale.deployed();
@@ -77,17 +77,17 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             let checkAmountTokensForSale = Math.floor(totalSupply * 40 / 100);
 
             try {
-                        await crowdsale.init();
-                    } catch(e) {
-                        console.log('!!! Already INIT() !!!');
-                    }
+                await crowdsale.init();
+            } catch(e) {
+                console.log('!!! Already INIT() !!!');
+            }
 
             console.log(amountTokensForSale);
             console.log(totalSupply);
             console.log(checkAmountTokensForSale);
 
             assert.isTrue(amountTokensForSale == checkAmountTokensForSale, 'amountTokensForSale() isn\'t 40% of total supply of tokens');
-        })
+        });
 
         it('amountTokensForOwners() should be the rest of total supply of tokens after sale for investors', async function(){
             let crowdsale = await Crowdsale.deployed();
@@ -103,7 +103,7 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             console.log(checkAmountTokensForOwners);
 
             assert.isTrue(amountTokensForOwners == checkAmountTokensForOwners, 'amountTokensForOwners() isn\'t the rest of total supply of tokens after sale for investors');
-        })
+        });
 
         it('limitOfFirstBuyers() should be 5', async function(){
             let crowdsale = await Crowdsale.deployed();
@@ -112,8 +112,8 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             //console.log(limitOfFirstBuyers);
 
             assert.isTrue(limitOfFirstBuyers == 5, 'limitOfFirstBuyers() isn\'t 5');
-        })
-    })
+        });
+    });
 
     describe('Check isIcoEnd()', function() {
         it('When tokens was NOT sold isIcoEnd() should be false', async function(){
@@ -127,12 +127,25 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             }
 
             assert.isTrue(isICOEnd == false, 'When tokens was NOT sold isIcoEnd() isn\'t false');
-        })
+        });
 
         it('When some amount of tokens was sold isIcoEnd() should be false', async function(){
-            let crowdsale = await Crowdsale.deployed();
+            //let crowdsale = await Crowdsale.deployed();
+            let crowdsale = await Crowdsale.new();
+
+            try {
+                await crowdsale.init();
+            } catch(e) {
+                console.log(e);
+                console.log('!!! Already INIT() !!!');
+            }
+
+            let owner = accounts[0];
+            await crowdsale.send(web3.toWei(10), {from: owner})
 
             let priceOfOneToken = web3.toWei(0.25);
+//            console.log(priceOfOneToken);
+//            console.log(20 * priceOfOneToken);
             await crowdsale.send(20 * priceOfOneToken, {from: investor});
 
             console.log('test');
@@ -145,37 +158,37 @@ contract('Crowdsale (initialized and ICO is continuing)', function(accounts) {
             }
 
             assert.isTrue(isICOEnd == false, 'When some amount of tokens was sold isIcoEnd() isn\'t false');
-        })
+        });
 
         it('When tokens was sold isIcoEnd() should be true', async function(){
             let crowdsale = await Crowdsale.deployed();
             //assert.isTrue(limitOfFirstBuyers == 5, ' isn\'t ');
-        })
-    })
+        });
+    });
 
     describe('Check isIcoFail()', function() {
         it(' should be ', async function(){
                 //assert.isTrue(limitOfFirstBuyers == 5, ' isn\'t ');
-        })
-    })
+        });
+    });
 
     describe('Check invest()', function() {
         it(' should be ', async function(){
                 //assert.isTrue(limitOfFirstBuyers == 5, ' isn\'t ');
-        })
-    })
+        });
+    });
 
     describe('Check distributeTokensAmongOwners()', function() {
         it(' should be ', async function(){
             //assert.isTrue(limitOfFirstBuyers == 5, ' isn\'t ');
-        })
-    })
+        });
+    });
 
     describe('Check refund()', function() {
         it(' should be ', async function(){
                 //assert.isTrue(limitOfFirstBuyers == 5, ' isn\'t ');
-        })
-    })
+        });
+    });
 });
 
 //contract('Crowdsale (initialized and ICO is ended by time)', function(accounts) {
