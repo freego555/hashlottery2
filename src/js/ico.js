582417859db3ab4269c5eed735d8c2714e8162f7
Mary Ushenina <mary.ushenina@gmail.com>
2018-11-03 04:38:52 +0200

    test sendSignedTransaction

--- src/js/ico.js
+++ src/js/ico.js
@@ -1,14 +1,21 @@
 import Web3 from 'web3'
-import TokenERC20JSON from '../../smart_contracts/build/contracts/TokenERC20'
+// import TokenERC20JSON from '../../smart_contracts/build/contracts/TokenERC20'
+// const tokenAbi = TokenERC20JSON.abi
+//
+// import CrowdsaleJSON from '../../smart_contracts/build/contracts/Crowdsale'
+// const CrowdsaleAbi = CrowdsaleJSON.abi
 
-const tokenAbi = TokenERC20JSON.abi
+
+var Crowdsale_address = '0x8cdb757f8eadac1dbe5062240cd8c920d8e4303a' // from test local
 
 /**
  * connect
  */
 if (typeof window.web3 !== 'undefined') {
-  window.web3 = new Web3(web3.currentProvider)
+  console.log("Using web3 detected from external source like Metamask");
+  window.web3 = new Web3(window.web3.currentProvider)
 } else {
+  console.log("Using localhost");
   // Set the provider you want from Web3.providers
   window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 }
@@ -16,7 +23,45 @@ if (typeof window.web3 !== 'undefined') {
 /** Identification Page */
 
 function invest () {
-  console.log('invest')
+  console.log('invest inside')
+
+  // var privateKeySaved = sessionStorage.getItem('privateKey');
+  var privateKeySaved = '0xa95fcd0643446b359ef39e35a22316380c4a9fe52201fceba095e33677efddbc'; // from local
+  var privateKeySaved = 'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109'; // from local
+  // var addressSaved = sessionStorage.getItem('address');
+
+
+  var Tx = require('ethereumjs-tx');
+  var privateKey = new Buffer('a95fcd0643446b359ef39e35a22316380c4a9fe52201fceba095e33677efddbc', 'hex')
+
+  // web3.eth.sendTransaction({
+  //   from: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe',
+  //   to: '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe',
+  //   value: '1000000000000000'
+  // })
+
+  var rawTx = {
+    nonce: '0x11',
+    gasPrice: '0x09184e72a000',
+    gasLimit: '0x2710',
+    to: Crowdsale_address,
+    value: '0x1000001000000000',
+    // data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057'
+  }
+
+  var tx = new Tx(rawTx);
+  tx.sign(privateKey);
+
+  var serializedTx = tx.serialize();
+
+// console.log(serializedTx.toString('hex'));
+// 0xf889808609184e72a00082271094000000000000000000000000000000000000000080a47f74657374320000000000000000000000000000000000000000000000000000006000571ca08a8bbf888cfa37bbf0bb965423625641fc956967b81d12e23709cead01446075a01ce999b56a8a88504be365442ea61239198e23d1fce7d00fcfc5cd3b44b7215f
+
+  window.web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+    .on('receipt', console.log);
+
+ // see eth.getTransactionReceipt() for details
+
 }
 
 $(document).ready(function () {
@@ -99,6 +144,10 @@ $(document).ready(function () {
         loader.style.display = 'none'
         // invest();
         console.log('invest try')
+
+      }).then(function () {
+
+        invest();
 
       }).catch(function (e) {
 
