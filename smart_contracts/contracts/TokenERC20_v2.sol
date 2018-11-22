cc54c46d9c57d39abd745a187d8ff168794ca43a
Ihor Rohovets <freego555@gmail.com>
2018-11-22 08:31:14 +0200

    Change TokenERC20_v2.sol. Add migration from newTokenERC20.sol.

--- smart_contracts/contracts/TokenERC20_v2.sol
+++ smart_contracts/contracts/TokenERC20_v2.sol
@@ -30,4 +30,39 @@ contract TokenERC20_v2 is TokenERC20 {
             onlyIfAllowedTransferInDraw {
         super._transfer(_from, _to, _value);
     }
+
+    /******** FOR MIGRATION ********/
+    /*modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner of contract can call this");
+        _;
+    }
+
+    function setMigrationAgent(address _agent) external isNotSetMigrationAgent() onlyOwner() {
+        migrationAgent = _agent;
+    }
+
+    modifier isNotSetMigrationAgent(){
+        require(migrationAgent == address(0), "Migration Agent was set earlly");
+        _;
+    }*/
+
+    modifier isMigrationRun(){
+        require(migrationStatus == true, "Sorry, but we not detect migration to new token.");
+        _;
+    }
+    modifier isCallFromMigrationAgent(){
+        require(msg.sender == migrationAgent, "Call from NOT MigrationAgent Contract");
+        _;
+    }
+    function createTokens(address _to, uint _value) public isMigrationRun() isSetMigrationAgent() isItCallFormMigrationAgent(){
+        balanceOf[_to] += _value;
+        totalSupply += _value;
+        emit Transfer(migrationAgent, _to, _value);
+    }
+    // Set contract from here we get tokens and check it!
+    function finalizeMigration() public isCallFromMigrationAgent() {
+        migrationAgent = address(0);
+        migrationStatus = false;
+    }
+    /******** FOR MIGRATION ********/
 }
