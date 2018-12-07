2c3a126a5d93aea145ee1307f35b6be32a9fda24
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2018-12-07 17:25:29 +0200

    recursive gettting array of stockholder from TokenERC20 WIP

--- smart_contracts/contracts/LotteryIncomeWallet.sol
+++ smart_contracts/contracts/LotteryIncomeWallet.sol
@@ -10,8 +10,8 @@ contract Draw {
         owner = msg.sender;
     }
 
-    function initDistributing(address wallet_address) public {
-        LotteryIncomeWallet(wallet_address).initDistributing(currentDrawId++);
+    function initDistributing(address wallet_address, uint256 count) public {
+        LotteryIncomeWallet(wallet_address).initDistributing(currentDrawId++, count);
     }
 }
 
@@ -98,3 +98,116 @@ contract LotteryIncomeWallet {
     }
 
 }
+
+
+
+
+
+/** Other variant */
+pragma solidity ^0.4.24;
+import "./TokenERC20.sol";
+import "./Draw.sol";
+
+
+contract LotteryIncomeWallet {
+    address public owner;
+    mapping(address => uint256) holderIndexes;
+    address[] public stockHolders;
+    address[] public stockHoldersMock;
+    uint256 public stockHoldersCount;
+    address drawContractAddress = address(0);
+    address tokenERC20Address = address(0);
+    mapping(address => uint256) public dividentsAvailable; //[owner'sAddress][total_dividends_available]
+    mapping(address => mapping(uint256 => uint256)) public dividendsFromDraw; //[owner'sAddress][lottery_id][dividends]
+    mapping(uint256 => uint256) public drawIncome; //[lottery_id][total_income_from_this_lottery]
+    uint256 public total_persentage; //debugging
+
+    event GotPartStockHolders(uint256 _startIndex, uint256 _count, uint256 lotery_id);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier isTokenSet() {
+        require(tokenERC20Address == address(0), "TokenERC20 Contract Address already set");
+        _;
+    }
+
+    modifier isDrawSet() {
+        require(drawContractAddress == address(0), "Draw Contract Address already set");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner of contract can call this");
+        _;
+    }
+
+    modifier onlyDraw() {
+        require(msg.sender == drawContractAddress, "Only Draw contract can call this");
+        _;
+    }
+
+    modifier onlyStockHolders() {
+        require(dividentsAvailable[msg.sender] > 0, "Only Stock Holders can call this");
+        _;
+    }
+
+    function setToken(address _tokenAddress) public isTokenSet() {
+        tokenERC20Address = _tokenAddress;
+    }
+
+    function setDraw(address _drawAddress) public isDrawSet() {
+        drawContractAddress = _drawAddress;
+    }
+
+    function getOwnersListIndex(address _ownerAddress) public view returns(uint256) {
+        return TokenERC20(tokenERC20Address).ownersIndex(_ownerAddress);
+    }
+
+    // функция получает часть массива акционеров из Токена-20 и начисляет этой части дивиденды
+    function getOwnerAddressesListPart(uint256 _startIndex, uint256 _count, uint256 lotery_id) public {
+
+        if ((_startIndex + _count) <= stockHoldersCount) {
+            stockHolders = TokenERC20(tokenERC20Address).getOwnerAddressesList(_startIndex, _startIndex + _count);
+            distributeIncome(stockHolders, lotery_id);
+
+            getOwnerAddressesListPart(_startIndex + _count, _count, lotery_id);
+        } else {
+            stockHolders = TokenERC20(tokenERC20Address).getOwnerAddressesList(_startIndex, stockHoldersCount);
+            distributeIncome(stockHolders, lotery_id);
+        }
+    }
+
+    // данная функция  распределяет прибыль от лотереи, путем определения текущей доли каждого акционера и начисления части прибыли от текущего розыгрыша согласно доле
+    function distributeIncome(address[] stockHoldersPart, uint256 lotery_id) public {
+        for (uint256 index = 0; index < stockHoldersPart.length; index++) {
+            uint256 owned = TokenERC20(tokenERC20Address).getOwnership(stockHoldersPart[index]);
+            total_persentage += owned;
+            dividendsFromDraw[stockHoldersPart[index]][lotery_id] = owned * drawIncome[lotery_id] / 1000000;
+        }
+    }
+
+    // Функция, которая запускает рекурсивное получение массива акционеров частями из Токена-20
+    function initDistributing(uint256 lotery_id, uint256 count) public  {
+        stockHoldersCount = TokenERC20(tokenERC20Address).getOwnerAddressesListLength();
+        getOwnerAddressesListPart(1, count, lotery_id);
+
+    }
+
+    function trackPayments() private {
+        drawIncome[Draw(drawContractAddress).currentDrawId()] += msg.value;
+
+    }
+
+    function () payable public {
+        trackPayments();
+    }
+}
+
+0x14723a09acff6d2a60dcdf7aa4aff308fddc160c //3
+0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db //5997
+0x583031d1113ad414f02576bd6afabfb302140225 //14000
+0xdd870fa1b7c4700f2bd7f44238821c26f7392148 //16000
+0x4aa7f9e01bf8015b9f75db95978209efef1c71da //24000
+0x46372a2d0cab03e983137ad289340f79cac18cf0 //40000
--- smart_contracts/contracts/TokenERC20.sol
+++ smart_contracts/contracts/TokenERC20.sol
@@ -178,3 +178,192 @@ contract TokenERC20 {
         return ownerAddressesLists[userIndex];
     }
 }
+
+
+
+
+// Other variant
+pragma solidity ^0.4.24;
+
+contract MigrationAgent {
+    function migrateFrom( address sender, uint256 _value) public;
+}
+
+contract TokenERC20 {
+    string public name; // Migrate ??
+    string public symbol; // Migrate ??
+    uint8 public decimals = 0; // Migrate ?
+    uint256 public totalSupply; // Migrate
+    mapping(address => uint256) public balanceOf; // Migrate
+    mapping(address => mapping(address => uint256)) public allowance;
+    address public owner;
+    bool private isTokensSupplied = false;
+
+    // ownership definition
+    mapping(address => uint256) public ownersIndex;
+    address[] public ownerAddressesLists;
+    address[] public ownerAddressesListPart;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event TokensEmitted(uint256 tokensSupplyed);
+
+    // For migration ERC20
+    event Migrate(address indexed _from, address indexed _to, uint256 _value);
+    address public migrationAgent;
+    uint256 public totalMigrated;
+    bool migrationStatus = false;
+
+    modifier isNotSetMigrationAgent(){
+        require(migrationAgent == address(0), "Migration Agent was set earlly");
+        _;
+    }
+
+    modifier isSetMigrationAgent(){
+        require(migrationAgent != address(0), "Migration Agent wasn't set earlly");
+        _;
+    }
+
+    modifier isValue(uint _value){
+        require(_value > 0, "Value can't should be grate than 0");
+        _;
+    }
+
+    modifier isValueGTBalance(uint _value, address _sender){
+        require(_value <= balanceOf[_sender], "Value can't be less than 0");
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner of contract can call this");
+        _;
+    }
+
+    modifier isMigrationNotRun(){
+        require(migrationStatus != true, "Sorry, but we detect migration to new token. You must miggrate all your tokens to new ERC20 token");
+        _;
+    }
+
+    modifier isItCallFormMigrationAgent(){
+        require(msg.sender == migrationAgent, "Sorry, its calls only from migrationAgent");
+        _;
+    }
+
+    function setMigrationAgent(address _agent) external isNotSetMigrationAgent() onlyOwner() {
+        migrationAgent = _agent;
+    }
+
+    function setMigrationStatus() public isSetMigrationAgent() isItCallFormMigrationAgent(){
+        if(!migrationStatus){
+            migrationStatus = true;
+        }
+    }
+
+    // User calls function for migrate his tokens to new ERC20 token
+    function migrateTokens(uint _value) external
+    isSetMigrationAgent()
+    isValue(_value)
+    isValueGTBalance(_value, msg.sender){
+
+        balanceOf[msg.sender] -= _value;
+        totalSupply -= _value;
+        totalMigrated += _value;
+        MigrationAgent(migrationAgent).migrateFrom(msg.sender, _value);
+        emit Migrate(msg.sender, migrationAgent, _value);
+    }
+
+    constructor() public {
+        name = "LotStock";
+        symbol = "L$";
+        owner = msg.sender;
+    }
+
+    function supplyTokens(address contract_address) public returns (bool success) {
+        require(owner == msg.sender, "Not Authorized");
+        require(isTokensSupplied == false, "Tokens already supplied");
+        totalSupply = 100000;
+        balanceOf[contract_address] = totalSupply;
+        isTokensSupplied = true;
+        ownersIndex[contract_address] = 0;
+        ownerAddressesLists.push(contract_address);
+        emit TokensEmitted(totalSupply);
+        return true;
+    }
+
+    function emitMoreTokens(uint256 tokens, address receiver) public isMigrationNotRun() returns (bool success) {
+        require(owner == msg.sender, "Not Authorized");
+        totalSupply += tokens;
+        balanceOf[receiver] += tokens;
+        if(ownersIndex[receiver] == 0) {
+            ownerAddressesLists.push(receiver);
+            ownersIndex[receiver] = ownerAddressesLists.length - 1;
+        }
+        emit TokensEmitted(tokens);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal isMigrationNotRun() {
+        require(isTokensSupplied == true, "Tokens are not supplied");
+        require(balanceOf[_from] >= _value, "Not enough funds");
+        require(_from != _to, "Transfering on same account is not allowed!");
+        require(_to != address(0), "Try send funds to 0-address");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        if (balanceOf[_from] == 0 && ownerAddressesLists[0] != _from) {
+            uint256 lastElemIndex = ownerAddressesLists.length - 1;
+            address lastElem = ownerAddressesLists[lastElemIndex];
+            if (lastElem != _from) {
+                uint256 senderIndex = ownersIndex[_from];
+                ownerAddressesLists[senderIndex] = lastElem;
+                ownersIndex[lastElem] = senderIndex;
+                ownerAddressesLists.length = lastElemIndex;
+            } else {
+                ownerAddressesLists.length = lastElemIndex;
+            }
+        }
+
+        if (ownersIndex[_to] == 0) {
+            ownersIndex[_to] = ownerAddressesLists.length;
+            ownerAddressesLists.push(_to);
+        }
+
+        emit Transfer(_from, _to, _value);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public
+    returns (bool success) {
+        require(_value <= allowance[_from][msg.sender], "User is not allowed to perform action");
+        allowance[_from][msg.sender] = _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public
+    returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // возвращает процент с четырьмя знаками после точки если разделить полученный результат на 10000
+    function getOwnership(address account) public view returns (uint256 percentageMultiplied10000) {
+        return balanceOf[account] * 1000000 / totalSupply;
+    }
+
+    function getOwnerAddressesList(uint256 _startIndex, uint256 _endIndex) public returns (address[]) {
+        for (uint256 index = _startIndex; index < _endIndex; index++) {
+            ownerAddressesListPart.push(ownerAddressesLists[index]);
+        }
+        return ownerAddressesListPart;
+    }
+
+    function getOwnerAddressesListLength() public view returns (uint256) {
+        return ownerAddressesLists.length;
+    }
+}
